f8862e825689a4c5303d26be2ed736e31666ae47
7forz <7f@7forz.com>
2020-05-23 22:19:48 +0800

    稍微重构获取行情的代码，并修改对应文档

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 # technical_indicator_calculator
 
-使用tushare获取A股给定股票代码的K线数据，使用futu获取港股，使用yfinance获取美股给定代码的K线数据（需先配置好[Futu OpenD](https://github.com/FutunnOpen/py-futu-api)）。可以计算给定参数的MA、MACD、RSI、KDJ、MTM、CCI等指标，使用方法见demo.py文件
+使用tushare获取A股给定股票代码的K线数据，使用futu获取港股（需先配置好[Futu OpenD](https://github.com/FutunnOpen/py-futu-api)），使用yfinance获取美股给定代码的K线数据。可以计算给定参数的MA、MACD、RSI、KDJ、MTM、CCI等指标，使用方法见demo.py文件
 
-# 运行环境:
+## 运行环境
 
-* Python 3 (3.7+ recommeded)
-* numpy 1.11.2+
-* pandas 0.19.2+
+* Python 3.6+
+* numpy
+* pandas
 * matplotlib
-* tushare 0.8.2+
+* tushare (for CN market)
+* futu-api (for HK/CN market)
 * yfinance (for US market)
-* numba (tested with 0.45)
+* numba (tested with 0.47)
--- global_data.py
+++ global_data.py
@@ -69,14 +69,62 @@ if futu_enabled:
     quote_ctx = futu.OpenQuoteContext(host='127.0.0.1', port=11111)
     print('Getting basic F10 info for HK..')
     return_code, data_df = quote_ctx.get_stock_basicinfo(futu.Market.HK, stock_type=futu.SecurityType.STOCK)
-    assert return_code == 0, 'get data from futu error: %s' % data_df
+    assert return_code == 0, f'get data from futu error: {data_df}'
     BASIC_INFO['HK'] = data_df.set_index('code')
 
     print('Getting basic F10 info for US..')
     return_code, data_df = quote_ctx.get_stock_basicinfo(futu.Market.US, stock_type=futu.SecurityType.STOCK)
-    assert return_code == 0, 'get data from futu error: %s' % data_df
+    assert return_code == 0, f'get data from futu error: {data_df}'
     BASIC_INFO['US'] = data_df.set_index('code')
     quote_ctx.close()
+
+
+def get_data_from_tushare(stock: str, start: str) -> pd.DataFrame:
+    df = ts.get_k_data(stock, start).set_index('date')  # tushare返回的是以数字作为索引 改成按日期索引
+
+    # 坑爹数据源 如果传入了start 就会获取不到今天的数据 解决方法：另外不传入start获取一次 再拼接最后一天的数据
+    df_without_start_param = ts.get_k_data(stock).set_index('date')
+
+    latest_date_df = df.index[-1]  # str
+    latest_date_df_without_start_param = df_without_start_param.index[-1]  # str
+    if latest_date_df != latest_date_df_without_start_param:
+        assert df.index[-1] == df_without_start_param.index[-2]  # 确认只少了最新的一天
+        latest_row = df_without_start_param.iloc[-1]
+        df = df.append(latest_row)
+    return df
+
+def get_data_from_tushare_pro(stock: str, start: str) -> pd.DataFrame:
+    start_yyyymmdd = start.replace('-', '')
+    df: pd.DataFrame = ts.pro_bar(ts_code=stock, adj='qfq', start_date=start_yyyymmdd)
+    df.rename(columns={'vol': 'volume'}, inplace=True)
+    df.sort_values(by='trade_date', inplace=True)  # ts pro的日期是反序的
+    df['trade_date'] = df['trade_date'].apply(lambda s: f'{s[:4]}-{s[4:6]}-{s[6:8]}')
+    df.set_index('trade_date', inplace=True)  # 改成按日期索引
+    return df
+
+def get_data_from_futu_opend(stock: str, start: str) -> pd.DataFrame:
+    quote_ctx = futu.OpenQuoteContext(host='127.0.0.1', port=11111)
+    now_date = time.strftime('%Y-%m-%d')  # 以现在时间为准 NEWEST_TRADE_DATE只适用于中国市场
+    return_code, df, _ = quote_ctx.request_history_kline(stock, start=start, end=now_date)
+    assert return_code == 0, f'get data from futu error: {df}'
+
+    # 返回的日期格式为'yyyy-mm-dd 00:00:00' 把后面的去掉 与tushare返回的格式保持统一
+    df['time_key'] = df['time_key'].apply(lambda s: s.split(' ')[0])
+    df.set_index('time_key', inplace=True)  # 改成按日期索引
+    quote_ctx.close()
+    return df
+
+def get_data_from_yfinance(stock: str, start: str) -> pd.DataFrame:
+    ticker = yf.Ticker(stock[3:])  # remove 'US.'
+    df = ticker.history(start=start)
+    df.columns = ['open', 'high', 'low', 'close', 'volume', 'Dividends', 'Stock Splits']  # 改为小写以统一
+
+    # 返回的日期格式为DatetimeIndex对象 转换为字符串
+    py_datetime_index = df.index.to_pydatetime()
+    datetime_to_str = np.vectorize(lambda s: s.strftime('%Y-%m-%d'))
+    date_only_array = datetime_to_str(py_datetime_index)
+    df.index = date_only_array
+    return df
 
 
 def add_data(stock: str, start: str) -> pd.DataFrame:  # 格式:1月必须写作01  2019-01-01
@@ -89,49 +137,18 @@ def add_data(stock: str, start: str) -> pd.DataFrame:  # 格式:1月必须写作
         2016-11-14  000001  8.883   8.941   8.970   8.883   975078.0
     """
     if stock.isdigit():  # 如果是纯数字 则调用tushare的沪深数据接口
-        new_df = ts.get_k_data(stock, start).set_index('date')  # tushare返回的是以数字作为索引 改成按日期索引
-
-        # 坑爹数据源 如果传入了start 就会获取不到今天的数据 解决方法：另外不传入start获取一次 再拼接最后一天的数据
-        new_df_without_start_param = ts.get_k_data(stock).set_index('date')
-
-        latest_date_new_df = new_df.index[-1]  # str
-        latest_date_new_df_without_start_param = new_df_without_start_param.index[-1]  # str
-        if latest_date_new_df != latest_date_new_df_without_start_param:
-            assert new_df.index[-1] == new_df_without_start_param.index[-2]  # 确认只少了最新的一天
-            latest_row = new_df_without_start_param.iloc[-1]
-            new_df = new_df.append(latest_row)
-    elif stock.endswith('.SH') or stock.endswith('.SZ'):  # .SH .SZ 结尾 则调用tushare pro   需要先设置其token
-        start_yyyymmdd = start.replace('-', '')
-        new_df: pd.DataFrame = ts.pro_bar(ts_code=stock, adj='qfq', start_date=start_yyyymmdd)
-        new_df.rename(columns={'vol': 'volume'}, inplace=True)
-        new_df.sort_values(by='trade_date', inplace=True)
-        new_df['trade_date'] = new_df['trade_date'].apply(lambda s: f'{s[:4]}-{s[4:6]}-{s[6:8]}')
-        new_df.set_index('trade_date', inplace=True)  # 改成按日期索引
-    elif stock.startswith('HK.'):  # 若代码包含英文字符 则调用futu的接口
-        quote_ctx = futu.OpenQuoteContext(host='127.0.0.1', port=11111)
-        now_date = time.strftime('%Y-%m-%d')  # 以现在时间为准 NEWEST_TRADE_DATE只适用于中国市场
-        return_code, new_df, _ = quote_ctx.request_history_kline(stock, start=start, end=now_date)
-        assert return_code == 0, 'get data from futu error: %s' % new_df
-
-        # 返回的日期格式为'yyyy-mm-dd 00:00:00' 把后面的去掉 与tushare返回的格式保持统一
-        new_df['time_key'] = new_df['time_key'].apply(lambda s: s.split(' ')[0])
-        new_df.set_index('time_key', inplace=True)  # 改成按日期索引
-        quote_ctx.close()
+        df = get_data_from_tushare(stock, start)
+    elif stock.endswith('.SH') or stock.endswith('.SZ'):  # 调用tushare pro  需要先设置其token
+        df = get_data_from_tushare_pro(stock, start)
+    elif stock.startswith('SH.') or stock.startswith('SZ.') or stock.startswith('HK.'):
+        df = get_data_from_futu_opend(stock, start)
     elif stock.startswith('US.'):
-        ticker = yf.Ticker(stock[3:])  # remove 'US.'
-        new_df = ticker.history(start=start)
-        new_df.columns = ['open', 'high', 'low', 'close', 'volume', 'Dividends', 'Stock Splits']  # 改为小写以统一
-
-        # 返回的日期格式为DatetimeIndex对象 转换为字符串
-        py_datetime_index = new_df.index.to_pydatetime()
-        datetime_to_str = np.vectorize(lambda s: s.strftime('%Y-%m-%d'))
-        date_only_array = datetime_to_str(py_datetime_index)
-        new_df.index = date_only_array
+        df = get_data_from_yfinance(stock, start)
     else:
         raise RuntimeError('Unknown stock code: {}'.format(stock))
 
     global stocks
-    stocks[stock] = pd.concat([stocks.get(stock), new_df])  # 注意原stocks[stock]可能为空 用concat合并数据
+    stocks[stock] = pd.concat([stocks.get(stock), df])  # 注意原stocks[stock]可能为空 用concat合并数据
     stocks[stock].drop_duplicates(subset=['close', 'volume'], inplace=True) # 用收盘价和成交量去掉重复数据(默认全部会导致对新数据去重失效) 而日期是索引不能用
     stocks[stock].sort_index(inplace=True, kind='mergesort')  # 确保添加后时间有序
     return stocks[stock]
@@ -142,7 +159,6 @@ def get_data(stock: str) -> pd.DataFrame:
     if result is not None:
         return result
     else:
-        # print('%s data does not exist, will download start from %s' % (stock, START_DOWNLOAD_DATE))
         return add_data(stock, START_DOWNLOAD_DATE)
 
 def add_column(stock, column, data) -> pd.DataFrame:
@@ -153,13 +169,17 @@ def add_column(stock, column, data) -> pd.DataFrame:
 
 def save_database(filename=DB_FILE):
     """ 保存当前数据到文件中 """
-    with open('%s' % filename, 'wb') as f:
+    with open(filename, 'wb') as f:
         pickle.dump(stocks, f, pickle.HIGHEST_PROTOCOL)
 
 def get_name(stock: str) -> str:
     """ 获取对应代码的中文名称 """
     if stock.isdigit() and stock in BASIC_INFO['CN'].index:  # 有些ETF基金会没有F10数据
         return BASIC_INFO['CN'].loc[stock]['name']
+    elif (stock.endswith('.SH') or stock.endswith('.SZ')) and stock[:-3] in BASIC_INFO['CN'].index:
+        return BASIC_INFO['CN'].loc[stock[:-3]]['name']
+    elif (stock.startswith('SH.') or stock.startswith('SZ.')) and stock[3:] in BASIC_INFO['CN'].index:
+        return BASIC_INFO['CN'].loc[stock[3:]]['name']
     elif stock.startswith('HK') and 'HK' in BASIC_INFO and stock in BASIC_INFO['HK'].index:
         return BASIC_INFO['HK'].loc[stock]['name']
     elif stock.startswith('US') and 'US' in BASIC_INFO and stock in BASIC_INFO['US'].index:
